4e64370e29212ca6b1239c85642d20719e87e169
Your Name <you@example.com>
2021-04-15 18:11:40 +0300

    first commit

---
+++ Requests/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: "https://api.openweathermap.org/data/2.5/weather",
+    appid: "d90e9570355188094b55bc4c9b6ea3e9"
+}
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.PORT || 27665,
+    url: 'mongodb+srv://DimonStad:TKD161823@cluster0.m7ovn.mongodb.net/WebPogodaDB?retryWrites=true&w=majority'
+}
