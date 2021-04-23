592bf693c9476b6f6f2b0df062250a1cca4dc83a
DimonStad <57269090+DimonStad@users.noreply.github.com>
2021-04-23 20:45:22 +0300

    Delete2

--- citiesWeather.js
+++
@@ -1,8 +0,0 @@
-const weather = require("./getWeather");
-const favourites = require("./listCities");
-
-function serverMethods(serv, mongoose) {
-    weather(serv);
-    favourites(serv, mongoose);
-}
-module.exports.citiesWeather = serverMethods;
